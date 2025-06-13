5d1fec6dfe995af992c9be4e3b2e5ab96c0610bd
cjezio <mauriciowoitkowitzmonroy@gmail.com>
2025-06-13 12:52:24 -0400

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="miQuintoRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Mauricio Woitkowitz",                         # Tu nombre
+    author_email="mauriciowoitkowitzmonroy@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/maurwoit/miQuintoRepo",     # URL del proyecto
+)
